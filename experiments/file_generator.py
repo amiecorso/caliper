cca8a54416c5fbc2ba2cb3ee225841e4d70bb306
amiecorso <acorso@cs.uoregon.edu>
2019-02-23 14:41:25 -0800

    first version file generator written

--- experiments/file_generator.py
+++ experiments/file_generator.py
@@ -1,19 +1,33 @@
 import argparse
+import os
 
 parser = argparse.ArgumentParser(description="Generate files for creating Sawtooth network of specified size, from specified template.")
 parser.add_argument('--n', default=1, type=int, help="The number of validators to create")
 parser.add_argument('--template', default='/Users/amiecorso/caliper/experiments/templates/sawtooth-poet.yaml', help="The template file from which to create validators")
+parser.add_argument('--dest', default='/Users/amiecorso/caliper/experiments/test/', help="Destination directory for output file")
 args = parser.parse_args()
+
+if not os.path.exists(args.dest):
+    os.mkdir(args.dest)
+
+output_file = str(args.n) + "_" + args.template.split('/')[-1]
+
+header_and_genesis = []
 
 with open(args.template, 'r') as template:
     template.readline() # move past header
-    lines = template.readlines()
+    line = template.readline()
+    while "end genesis" not in line:
+        header_and_genesis.append(line)        
+        line = template.readline()
 
-header_lines = []
-for line in lines:
-    if "end header" in line:
-        break
-    header_lines.append(line)
+    format_body = template.read() # read the rest of the file as format body
 
 
-print("HEADER: ", header_lines)
+with open(args.dest + output_file, 'w') as output:
+    output.write("# " + output_file + "\n")
+    output.writelines(header_and_genesis)
+    for i in range(1, args.n):
+        next_validator = format_body.replace("{}", str(i))
+        output.write(next_validator)
+
