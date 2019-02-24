ee9d966007cacfdf582a54020cee00ffb76c39c8
amiecorso <acorso@cs.uoregon.edu>
2019-02-24 15:43:07 -0800

    top level tweak

--- experiments/run_exp.py
+++ experiments/run_exp.py
@@ -9,13 +9,16 @@ NETCONFIG_TEMPLATE = "~/caliper/experiments/templates/net_config_template.json"
 TPFAMILY = "testTP"
 BBFILE = "TestBatchBuilder.js"
 
+# generate compose files
 for n in NET_SIZES:
     command = "python compose_file_gen.py --n {} --template {} --dest {}".format(n, COMPOSE_TEMPLATE, EXP_DIR + "/compose_files")
     subprocess.call(command, shell=True)
 
-
+# generate netconfig files
 for n in NET_SIZES:
     command = "python netconfig_file_gen.py --n {} --template {} --dest {} --exp_dir {} --TPfamily {} --bb_file {}".format(n, NETCONFIG_TEMPLATE, EXP_DIR + "/net_config_files", EXP_DIR, TPFAMILY, BBFILE)
     subprocess.call(command, shell=True)
+
+# deliver workload to each network
 
 
