a3b1c1b9e5edc0b52224d8896f764f47e12e10e9
amiecorso <acorso@cs.uoregon.edu>
2019-03-05 17:22:40 -0800

    debugging poet-cft problems

--- experiments/stale_blocks/deliver_workload.py
+++ experiments/stale_blocks/deliver_workload.py
@@ -13,9 +13,10 @@ REST_URL_FORMATTER = "http://sawtooth-rest-api-default-{}:8008"
 docker_client = docker.from_env()
 client = docker_client.containers.get("sawtooth-shell-default")
 
-for i in range(100):
+for i in range(10):
     which_val = random.randint(0, 4)
     intkey_command =  "intkey set --url " + REST_URL_FORMATTER.format(which_val) + " key" + str(i) + " " + str(i)
+    print("command: ", intkey_command)
     client.exec_run(intkey_command)
 
 docker_client.close()
