cca23f03d0d80d84fe3f82547f1420a0fe20c8c6
amiecorso <acorso@cs.uoregon.edu>
2019-03-10 10:41:10 -0700

    poet 1.0 template working

---
+++ experiments/poet_intkey_1.0/run_intkey.sh
@@ -0,0 +1,2 @@
+node ~/caliper/benchmark/intkey/main.js -c ~/caliper/experiments/dev_intkey_1.1/config-saw-intkey.yaml -n ~/caliper/experiments/dev_intkey_1.1/net_config_files/netconfig-official.json
+
