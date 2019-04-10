c2f54c5a9ae4fa69b52ba97b2f000d3e5935d208
amiecorso <acorso@cs.uoregon.edu>
2019-04-10 12:03:18 -0700

    fixed data processing

--- experiments/data_scripts/process_exp.py
+++ experiments/data_scripts/process_exp.py
@@ -10,60 +10,81 @@ args = parser.parse_args()
 
 results_dir = args.exp_dir.rstrip("/") + "/results/"
 run_exp_path = args.exp_dir.rstrip("/") + "/run_exp.py"
-PERFORMANCE_SUMMARY = results_dir + "/performance_summary.csv"
-RESOURCE_SUMMARY = results_dir + "/resource_summary.csv"
-
+PERFORMANCE_SUMMARY = results_dir + "performance_summary.csv"
+RESOURCE_SUMMARY = results_dir + "resource_summary.csv"
 
 with open(run_exp_path, 'r') as run_exp:
     contents = run_exp.readlines()
 
-# determine number of runs
+# determine REPEATS and NETSIZES from run_exp.py
 for line in contents:
     if "REPEATS" in line:
         REPEATS = int(line.split("=")[-1].strip())
         break
-
-# Traverse directory tree, read relevant files and incorporate extra information
-for size in os.listdir(results_dir):
-    sizedir = results_dir + size
-    for tps in os.listdir(sizedir):
-        rate = tps.rstrip('tps')
-        tpsdir = sizedir + "/"  + tps
-        for datafile in os.listdir(tpsdir):
-            for run in range(REPEATS):
-
-performance_files = [f for f in os.listdir(results_dir) if "performance" in f]
-resource_files = [f for f in os.listdir(results_dir) if "resource" in f]
-staleb_files = [f for f in os.listdir(results_dir) if "stale" in f]
-
-if not performance_files:
-    sys.exit("Error: there are no results")
-
-# process performance files
-os.chdir(results_dir)
+for line in contents:
+    if "NET_SIZES" in line:
+        NET_SIZES = line.split("=")[-1].strip().strip("[").strip("]").split(",")
+        break
+for i in range(len(NET_SIZES)):
+    NET_SIZES[i] = NET_SIZES[i].strip()
+NET_SIZES.sort(key=int)
+# Open summary files and write headers:
 perf_out = open(PERFORMANCE_SUMMARY, "w")
-    # write header
-with open(performance_files[0], 'r') as f:
-    perf_out.write("Network Size,Stale Block Rate," + f.readline())
-
-for num in netsizes:
-    pfile = [f for f in performance_files if f.startswith(num)][0]
-    sbfile = [f for f in staleb_files if f.startswith(num)][0]
-    with open(pfile, 'r') as f:
-        perf_lines = f.readlines()
-    with open(sbfile, 'r') as f:
-        sb_data = f.readlines()[0] # get just the block rate, ignore block list diagnostics
-    line = ",".join([num, sb_data.strip(), perf_lines[-1]])
-    perf_out.write(line)
-
-perf_out.close()
-
-# process resource consumption files
 resource_out = open(RESOURCE_SUMMARY, 'w')
-    # write header
-with open(resource_files[0], 'r') as f:
-    resource_out.write("Network Size," + f.readline())
+pheader = False
+rheader = False
+# Traverse directory tree, read relevant files and incorporate extra information
+for size in NET_SIZES:
+    sizedir = results_dir + size
+    rates = os.listdir(sizedir)
+    rates = [rate.rstrip("tps") for rate in rates]
+    rates.sort(key=int)
+    for tps in rates:
+        tpsdir = sizedir + "/"  + tps + "tps/"
+        performance_files = [tpsdir + f for f in os.listdir(tpsdir) if "performance" in f]
+        resource_files = [tpsdir + f for f in os.listdir(tpsdir) if "resource" in f]
+        staleb_files = [tpsdir + f for f in os.listdir(tpsdir) if "stale" in f]
+        analysis_files = [tpsdir + f for f in os.listdir(tpsdir) if "analysis" in f]
+        # Write headers
+        try:
+            if not pheader:
+                with open(performance_files[0], 'r') as f:
+                    perf_out.write("Network Size,Run Index," + f.readline().rstrip('\n') + ",Stale Block Rate\n")
+            pheader = True
+        except Exception as e:
+            print(e)
+            print("Error: missing Performance results?")
+            sys.exit()
+        try:
+            if not rheader:
+                with open(resource_files[0], 'r') as f:
+                    resource_out.write("Network Size," + f.readline())
+            rheader = True
+        except Exception as e:
+            print(e)
+            print("Error: missing Performance results?")
+            sys.exit()
 
+        '''
+        print(performance_files)
+        print(resource_files)
+        print(staleb_files)
+        print(analysis_files)
+        '''
+        for run in range(REPEATS):
+            # process performance files + stale block files
+            pfile = [f for f in performance_files if f.endswith(str(run) + ".csv")][0]
+            sbfile = [f for f in staleb_files if f.endswith(str(run) + ".csv")][0]
+            with open(pfile, 'r') as f:
+                perf_lines = f.readlines()
+            with open(sbfile, 'r') as f:
+                sb_data = f.readlines()[0] # get just the block rate, ignore block list diagnostics
+            line = ",".join([size, str(run), perf_lines[-1].rstrip('\n'), str(round(float(sb_data.strip()), 3))]) #<-- only handles a ONE-line perf (one round)
+            perf_out.write(line + "\n")
+            # process resource files for this round
+            # TODO: eventually incorporate analysis files too
+'''
+# process resource consumption files
 for num in netsizes:
     resfile = [f for f in resource_files if f.startswith(num)][0]
     with open(resfile, 'r') as f:
@@ -71,6 +92,6 @@ for num in netsizes:
         res_lines = f.readlines() # read the rest
     for line in res_lines:
         resource_out.write(num + "," + line)
+'''
+perf_out.close()
 resource_out.close()
-
-
