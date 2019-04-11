68a5dab3d1be36af39b1996d5d52b338a67258fe
amiecorso <acorso@cs.uoregon.edu>
2019-04-11 12:25:41 -0700

    saving exp results

--- experiments/benchconfig_file_gen.py
+++ experiments/benchconfig_file_gen.py
@@ -13,6 +13,7 @@ parser.add_argument('--duration', default=30)
 parser.add_argument('--unfinished', default=5)
 parser.add_argument('--txnsperbatch', default=20)
 parser.add_argument('--label', default='onlyround')
+parser.add_argument('--rounds', default='single', help="If not single, indicate number of rounds that should be performed of given specification")
 args = parser.parse_args()
 
 if not args.exp_dir.endswith("/"):
@@ -20,14 +21,53 @@ if not args.exp_dir.endswith("/"):
 
 output_file = args.exp_dir + 'config-saw-intkey.yaml' 
 
-with open(args.template, 'r') as template:
-    format_body = template.read() # read the rest of the file as format body
+if args.rounds == 'single':
+    with open(args.template, 'r') as template:
+        format_body = template.read() # read the rest of the file as format body
+    format_body = format_body.replace("{beginrounds}", "")
+    format_body = format_body.replace("{endrounds}", "")
+    format_body = format_body.replace("{tps}", str(args.tps))
+    format_body = format_body.replace("{duration}", str(args.duration))
+    format_body = format_body.replace("{unfinished}", str(args.unfinished))
+    format_body = format_body.replace("{txnsperbatch}", str(args.txnsperbatch))
+    format_body = format_body.replace("{label}", args.label)
 
-format_body = format_body.replace("{tps}", str(args.tps))
-format_body = format_body.replace("{duration}", str(args.duration))
-format_body = format_body.replace("{unfinished}", str(args.unfinished))
-format_body = format_body.replace("{txnsperbatch}", str(args.txnsperbatch))
-format_body = format_body.replace("{label}", args.label)
+    with open(output_file, 'w') as output:
+        output.write(format_body)
 
-with open(output_file, 'w') as output:
-    output.write(format_body)
+else:
+    header = []
+    roundbody = []
+    footer = []
+    with open(args.template, 'r') as template:
+        line = template.readline()
+        while "{beginrounds}" not in line:
+            header.append(line)
+            line = template.readline()
+        while "{endrounds}" not in line:
+            roundbody.append(line)
+            line = template.readline()
+        for rest in template:
+            footer.append(rest)
+    header = "".join(header)
+    footer = "".join(footer)
+    format_body = "".join(roundbody)
+
+    format_body = format_body.replace("{tps}", str(args.tps))
+    format_body = format_body.replace("{duration}", str(args.duration))
+    format_body = format_body.replace("{unfinished}", str(args.unfinished))
+    format_body = format_body.replace("{txnsperbatch}", str(args.txnsperbatch))
+    format_body = format_body.replace("{label}", args.label)
+    format_body = format_body.replace("{beginrounds}", "")
+    format_body = format_body.replace("{endrounds}", "")
+    header = header.replace("{beginrounds}", "")
+    header = header.replace("{endrounds}", "")
+    footer = footer.replace("{beginrounds}", "")
+    footer = footer.replace("{endrounds}", "")
+
+    with open(output_file, 'w') as output:
+        output.write(header)
+        for i in range(int(args.rounds)):
+            output.write(format_body)
+        output.write(footer)
+
