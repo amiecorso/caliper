611261c52818ae4084ba2fa27c0fe70a8465b444
amiecorso <acorso@cs.uoregon.edu>
2019-04-15 19:51:41 -0700

    saving exp 04-15-19-sizes124-linear-gradient45-80-rep31

---
+++ experiments/arch_exps/04-15-19-sizes124-linear-gradient45-80-rep31/generators/netconfig_file_gen.py
@@ -0,0 +1,49 @@
+# netconfig_file_gen.py
+
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description="Generate network configuration file from specified template, pointing to composefile/network of specified size.")
+parser.add_argument('--n', default=1, type=int, help="The network size for this Caliper run")
+parser.add_argument('--template', default='../templates/net_config_template.json', help="The template file from which to create network config file.")
+parser.add_argument('--dest', default='/Users/amiecorso/caliper/experiments/test/', help="Destination directory for output file")
+parser.add_argument('--exp_dir', default='/Users/amiecorso/caliper/experiments/poet_prototest', help="The directory of this experiment")
+parser.add_argument('--TPfamily', default='donothing', help="The name of the transaction processor family for this workload")
+parser.add_argument('--bb_file', default='DoNothingBatchBuilder.js', help="The name of the .js BatchBuilder file for this transaction family, located in src/adapters/sawtooth/Application/")
+parser.add_argument('--run_num', help="The repeat we're on, for performing identical runs of same workload")
+parser.add_argument('--leave_up',default="False", help="Leave the Docker network running after workload delivery?")
+parser.add_argument('--tps')
+args = parser.parse_args()
+
+if not args.dest.endswith("/"):
+    args.dest = args.dest + "/"
+
+if not os.path.exists(args.dest):
+    os.mkdir(args.dest)
+
+base_filename = args.template.split('/')[-1].replace("_template", "") # get rid of 'template' tag
+output_file = str(args.n) + "_" + base_filename
+
+
+with open(args.template, 'r') as template:
+    format_body = template.read() # read the rest of the file as format body
+
+# assign rest-api url array
+restapi_array = "["
+for i in range(args.n):
+    next_port = str(8008 + i)
+    next_url = "http://127.0.0.1:" + next_port
+    restapi_array += "\"" + next_url + "\", "
+restapi_array = restapi_array.rstrip(", ") + "]"
+
+format_body = format_body.replace("{n}", str(args.n))
+format_body = format_body.replace("{exp_dir}", args.exp_dir)
+format_body = format_body.replace("{run_num}", args.run_num)
+format_body = format_body.replace("{leave_up}", str(args.leave_up))
+format_body = format_body.replace("{TPfamily}", args.TPfamily)
+format_body = format_body.replace("{batchbuilder_file}", args.bb_file)
+format_body = format_body.replace("{restapi_urls}", restapi_array)
+format_body = format_body.replace("{tps}", str(args.tps))
+
+with open(args.dest + output_file, 'w') as output:
+    output.write(format_body)
---
+++ experiments/arch_exps/04-15-19-sizes124-linear-gradient45-80-rep31/stale_blocks/save_logs.py
@@ -0,0 +1,56 @@
+# save_logs.py
+# Save logs for all containers in network - diagnostic
+
+import docker
+import time
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description="Save Docker container logs for scrutiny")
+parser.add_argument('--n', default=1, type=int, help="Network size")
+parser.add_argument('--exp_dir', help="Which experiment are we parsing?")
+parser.add_argument('--run_num', help="Which run of the experiment is this?")
+parser.add_argument('--tps')
+args = parser.parse_args()
+
+# make the output directory if it doesn't exist yet
+if not args.exp_dir.endswith("/"):
+    args.exp_dir += "/"
+
+LOGS_DIR = args.exp_dir + "LOGS/"
+if not os.path.exists(LOGS_DIR):
+    os.mkdir(LOGS_DIR)
+
+OUTPUT_DIR = LOGS_DIR + str(args.n) + "/"
+if not os.path.exists(OUTPUT_DIR):
+    os.mkdir(OUTPUT_DIR)
+
+TPS_DIR = OUTPUT_DIR + args.tps + "tps/"
+if not os.path.exists(TPS_DIR):
+    os.mkdir(TPS_DIR)
+
+RUN_DIR = TPS_DIR + "run" + args.run_num + "/"
+if not os.path.exists(RUN_DIR):
+    os.mkdir(RUN_DIR)
+
+compose_file = None
+for f in os.listdir(args.exp_dir + "compose_files"):
+    if (f.startswith(str(args.n))):
+        compose_file = f
+        break
+if not compose_file: # if we can't find file
+    raise Exception("There is no compose file for network size {}".format(str(args.n)))
+
+docker_client = docker.from_env()
+
+with open(args.exp_dir + "compose_files/" + compose_file, 'r') as f:
+    lines = f.readlines()
+
+for line in lines:
+    if "container_name" in line:
+        container_name = line.split(":")[-1].strip() # grab the container name from the .yaml file
+        container = docker_client.containers.get(container_name)
+        log = container.logs().decode('utf-8')
+        with open(RUN_DIR + "/" + container_name, 'w') as out:
+            out.write(log)
+
