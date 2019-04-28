71db2d48d4684674a3332f0ac91319f04534c8fb
amiecorso <acorso@cs.uoregon.edu>
2019-04-27 17:12:59 -0700

    asdf

--- experiments/poet_intkey_1.0_varyinterval/run_exp.py
+++ experiments/poet_intkey_1.0_varyinterval/run_exp.py
@@ -60,8 +60,7 @@ for n in NET_SIZES:
                 subprocess.call(command, shell=True)
 
                 # external monitor:
-                analysis_dest = EXP_DIR + "results" + "/"
-                analysis = "python3 {}data_scripts/analyze_network.py --n {} --dest {} --run_num {} --time {} --tps {} --interval {} ".format(EXP_DIR, n, analysis_dest, repeat, TIME, tps, interval)
+                analysis = "python3 {}data_scripts/analyze_network.py --n {} --dest {} --run_num {} --time {} --tps {} --interval {} ".format(EXP_DIR, n, EXP_DIR, repeat, TIME, tps, interval)
                 analysis = "ssh amie@{} ".format(REMOTEIP) + "\"" + analysis + "\""
                 print("Starting external analysis...")
                 print("executing command: ", analysis)
