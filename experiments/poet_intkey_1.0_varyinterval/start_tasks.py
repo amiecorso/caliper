f7e038b7a4e66ebc62e2896312ba79d63ea74985
amiecorso <acorso@cs.uoregon.edu>
2019-05-06 12:23:38 -0700

    changed call to popen

--- experiments/poet_intkey_1.0_varyinterval/start_tasks.py
+++ experiments/poet_intkey_1.0_varyinterval/start_tasks.py
@@ -20,4 +20,4 @@ compose_file = [f for f in os.listdir(args.exp_dir + "compose_files") if f.start
 start_network = "docker-compose -f {}compose_files/{} up -d && sleep 15".format(args.exp_dir, compose_file)
 print("Starting network... ")
 print("Executing command: ", start_network)
-subprocess.call(start_network, shell=True)
+subprocess.call(start_network.split())
