53abe29792df3f0494216d29178c4222f9ac46af
amiecorso <acorso@cs.uoregon.edu>
2019-05-14 14:09:52 -0700

    asdf

--- experiments/mauna_poet_intkey_1.0_varytxnperbatch/master.py
+++ experiments/mauna_poet_intkey_1.0_varytxnperbatch/master.py
@@ -1,7 +1,6 @@
 import subprocess
 
-#txnsperbatch = [1, 10, 20, 40, 100, 200]
-txnsperbatch = [7, 14]
+txnsperbatch = [1, 10, 20, 40, 100, 200]
 
 for tpb in txnsperbatch:
     subprocess.call("python3 run_exp.py --tperb {}".format(tpb), shell=True)
--- experiments/mauna_poet_intkey_1.0_varytxnperbatch/run_exp.py
+++ experiments/mauna_poet_intkey_1.0_varytxnperbatch/run_exp.py
@@ -15,15 +15,15 @@ args = parser.parse_args()
 #REMOTEIP = "192.168.0.105"
 TPERB = args.tperb
 REMOTEIP = "128.223.6.92"
-SAVE_AS = time.strftime("%m-%d-%y") + "mauna_TPB={}TEST".format(TPERB)
+SAVE_AS = time.strftime("%m-%d-%y") + "mauna_TPB{}".format(TPERB)
 INTERVALS = [20]
-NET_SIZES = [1]
+NET_SIZES = [1, 2, 4, 6, 8]
 REPEATS = 1
 TIME = 2000
 #           (TPS, duration, unfinished)
-#WORKLOADS = [(5, 800, 200), (10, 800, 200), (15, 800, 200), (20, 800, 200), (30, 800, 200), (40, 800, 200), (50, 800, 200), (60, 800, 200)]
+WORKLOADS = [(5, 800, 200), (10, 800, 200), (15, 800, 200), (20, 800, 200), (30, 800, 200), (40, 800, 200), (50, 800, 200), (60, 800, 200)]
 #WORKLOADS = [(5, 800, 200), (10, 800, 200), (15, 800, 200), (20, 800, 200), (25, 800, 200), (30, 800, 200), (40, 800, 200)]
-WORKLOADS = [(5, 40, 5)]
+#WORKLOADS = [(5, 40, 5)]
 #TIME = 2000 # maximum time to run external monitor... should at least be as long as the duration of experiment, otherwise monitor will come down early
 LEAVE_UP = False
 if LEAVE_UP: # if leaving the network running, can only handle one instance at a time
