43a593e79429806b1414a2f082efeda1ba166486
amiecorso <acorso@cs.uoregon.edu>
2019-05-20 01:38:11 -0700

    pushing from mac

--- experiments/mauna_poet_intkey_1.0_varyinterval/run_exp.py
+++ experiments/mauna_poet_intkey_1.0_varyinterval/run_exp.py
@@ -12,12 +12,12 @@ REMOTEIP = "128.223.6.92"
 SAVE_AS = time.strftime("%m-%d-%y") + "mauna_6_manyrepeats"
 INTERVALS = [20]
 NET_SIZES = [6]
-REPEATS = 100
+REPEATS = 15
 TIME = 2000
 #           (TPS, duration, unfinished)
-#WORKLOADS = [(5, 800, 200), (10, 800, 200), (15, 800, 200), (20, 800, 200), (25, 800, 200), (30, 800, 200), (35, 800, 200), (40, 800, 200), (45, 800, 200), (50, 800, 200)]
+WORKLOADS = [(5, 800, 200), (10, 800, 200), (15, 800, 200), (20, 800, 200), (25, 800, 200), (30, 800, 200), (35, 800, 200), (40, 800, 200), (45, 800, 200), (50, 800, 200)]
 #WORKLOADS = [(5, 800, 200), (10, 800, 200), (15, 800, 200), (20, 800, 200), (25, 800, 200), (30, 800, 200), (40, 800, 200)]
-WORKLOADS = [(5, 40, 5)]
+#WORKLOADS = [(5, 40, 5)]
 #TIME = 2000 # maximum time to run external monitor... should at least be as long as the duration of experiment, otherwise monitor will come down early
 LEAVE_UP = False
 if LEAVE_UP: # if leaving the network running, can only handle one instance at a time
